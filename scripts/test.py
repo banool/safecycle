c425a1b0dc740fc6e79a97c915497786ea2a8e4d
Daniel Porteous <danielporteous1@gmail.com>
2016-04-30 19:12:16 +1000

    First test at updating the gmaps iframe.

--- scripts/test.py
+++ scripts/test.py
@@ -3,4 +3,17 @@ cgitb.enable()
 
 print("Content-Type: text/html\n")
 
-print("Hi")
+print("""
+									<div class="row">
+										<iframe
+										  width="100%"
+										  height="750"
+										  frameborder="0" style="border:0"
+										  src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyADJzDYaO0we1opZUxxUULc8yFgD1W5nKo
+										    &origin=8+first+court+preston,3072
+										    &destination=NGV
+										    &waypoints=Museum+of+Melbourne
+										    &mode=bicycling" allowfullscreen>
+										</iframe>
+									</div>
+""")
