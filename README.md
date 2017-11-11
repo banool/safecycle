0d6174f18b9bd50d3640793c3579417b22415357
Daniel Porteous <danielporteous1@gmail.com>
2017-11-11 21:02:22 +1100

    http to https

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Safe Cycle
 Safe Cycle is the 1st place winning project our team created in less than 24 hours for the 2016 Melbourne Data Science Challenge, sponsored by Microsoft and Azure ML.
 
-Take a look at the project here: http://safecycle.xyz/
+Take a look at the project here: https://safecycle.xyz/
 
 ## Overview
 Safe Cycle is a web app that tries to create safer routes for cyclists. The approach we took uses Azure ML, Google Maps and some Python scripts with an HTML jQuery driven front end. Data related to past crashes and the time of day is used to try and avoid danger hot spots on a cyclist's journey, with room for additional data such as weather and traffic to be taken into account. The project was inspired by a cycling injury I had sustained where I fell off my bike and broke my collarbone (for which I was getting surgery the next morning), [check it out!](https://www.dropbox.com/sh/4ojal2l0ha7kf3r/AAC_VnPk-99WFY5O_PDhui3Da?dl=0)
@@ -14,7 +14,7 @@ Currently only requests for Canberra can be processed properly as we haven't loa
 ### Directory structure
 - **web** Contains the all the HTML for the front end, including relevant CSS and Javascript. No images were used, but they would be here too.
 - **scripts** Contains the Python scripts that communicate with Azure ML and create the Google Maps request.
-- **other** Contains the original template used for the front end (thanks to [Pixelarity!](http://pixelarity.com/)) as well as other unused / old working files.
+- **other** Contains the original template used for the front end (thanks to [Pixelarity!](https://pixelarity.com/)) as well as other unused / old working files.
 
 ## Technical implementation details
 This diagram loosely explains what happens when a user submits a request for a route map.
--- web/assets/jquery.form.js
+++ web/assets/jquery.form.js
@@ -3,7 +3,7 @@
  * version: 3.51.0-2014.06.20
  * Requires jQuery v1.5 or later
  * Copyright (c) 2014 M. Alsup
- * Examples and documentation at: http://malsup.com/jquery/form/
+ * Examples and documentation at: https://malsup.com/jquery/form/
  * Project repository: https://github.com/malsup/form
  * Dual licensed under the MIT and GPL licenses.
  * https://github.com/malsup/form#copyright-and-license
@@ -93,7 +93,7 @@ $.fn.attr2 = function() {
 $.fn.ajaxSubmit = function(options) {
     /*jshint scripturl:true */
 
-    // fast fail if nothing selected (http://dev.jquery.com/ticket/2752)
+    // fast fail if nothing selected (https://dev.jquery.com/ticket/2752)
     if (!this.length) {
         log('ajaxSubmit: skipping submit process - no element selected');
         return this;
@@ -240,7 +240,7 @@ $.fn.ajaxSubmit = function(options) {
     // 06-NOV-09: now defaulting to iframe mode if file input is detected
     if (options.iframe !== false && (options.iframe || shouldUseFrame)) {
         // hack to fix Safari hang (thanks to Tim Molendijk for this)
-        // see:  http://groups.google.com/group/jquery-dev/browse_thread/thread/36395b7ab510dd5d
+        // see:  https://groups.google.com/group/jquery-dev/browse_thread/thread/36395b7ab510dd5d
         if (options.closeKeepAlive) {
             $.get(options.closeKeepAlive, function() {
                 jqxhr = fileUploadIframe(a);
@@ -467,7 +467,7 @@ $.fn.ajaxSubmit = function(options) {
              * carry the protocol property in ie8, when running under ssl
              * frame.document is the only valid response document, since
              * the protocol is know but not on the other two objects. strange?
-             * "Same origin policy" http://en.wikipedia.org/wiki/Same_origin_policy
+             * "Same origin policy" https://en.wikipedia.org/wiki/Same_origin_policy
              */
 
             var doc = null;
@@ -862,7 +862,7 @@ $.fn.ajaxForm = function(options) {
             });
             return this;
         }
-        // is your DOM ready?  http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+        // is your DOM ready?  https://docs.jquery.com/Tutorials:Introducing_$(document).ready()
         log('terminating; zero elements found by selector' + ($.isReady ? '' : ' (DOM not ready)'));
         return this;
     }
@@ -1092,7 +1092,7 @@ $.fn.fieldSerialize = function(successful) {
  *  v == ['C1']
  *
  * The successful argument controls whether or not the field element must be 'successful'
- * (per http://www.w3.org/TR/html4/interact/forms.html#successful-controls).
+ * (per https://www.w3.org/TR/html4/interact/forms.html#successful-controls).
  * The default value of the successful argument is true.  If this value is false the value(s)
  * for each element is returned.
  *
